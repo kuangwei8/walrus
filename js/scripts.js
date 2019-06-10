96a7b1b6485a1845c1aeacd4a3a17cdaa798212d
Serhiy Savchuk and Wei Kuang <serhiy@>
2019-06-10 13:33:11 -0700

    edited file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
-  $("h1").click(function(event) {
-    alert("You clicked on header");
 
-    event.preventDefault();
+    $("body").click(function(event) {
+  // $("h1").click(function(event) {
+  //   alert("You clicked on header");
+  //
+  // $("p").click(function(event) {
+  //   alert("You clicked on par");
+  //
+  // $("img").click(function(event) {
+  //   alert("You clicked on pic");
+
+  var elements = ["h1","p", "img"];
+  elements.forEach(function(element){
+    // $(element).click(function(){
+    alert("You clicked on "+ element);
+
+
+      event.preventDefault();
+    });
   });
 });
